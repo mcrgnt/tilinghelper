d74850f9f2de2be33ad2beb9a9ed5f0d29accd1a
mcrgnt <microgiant@yandex.ru>
2023-12-23 15:05:39 +0300

    v0.0.0

--- src/go.mod
+++ src/go.mod
@@ -1,5 +1,3 @@
 module tilinghelper
 
 go 1.21.4
-
-require github.com/mcrgnt/w v0.0.2
--- src/main.go
+++ src/main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/mcrgnt/w"
-)
-
 func main() {
-	w.Test()
-	fmt.Println("testetest")
-	fmt.Println("version v0.0.4++")
 }
